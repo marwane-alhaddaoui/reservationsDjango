550a1599faa3eb18334a631530df07da787d48d7
marwane-alhaddaoui <m.alhaddaoui.000991@stu.brucity.education>
2024-11-10 21:05:54 +0100

    Prodile update

--- reservations/accounts/urls.py
+++ reservations/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserSignUpView,profile
+from .views import UserSignUpView,profile,UserUpdateView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', UserSignUpView.as_view(), name='user-signup'),
     path('profile/', profile, name='user-profile'),
+    path('profile/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
 ]
