aa11537e11b4d9562701b9db9a8b82dd4ca70281
marwane-alhaddaoui <m.alhaddaoui.000991@stu.brucity.education>
2024-11-10 06:03:00 +0100

    Form update Artist

--- reservations/catalogue/urls.py
+++ reservations/catalogue/urls.py
@@ -1,7 +1,6 @@
 """reservations.catalogue URL Configuration
 """
 from django.urls import path
-
 from . import views
 
 app_name='catalogue'
@@ -9,4 +8,5 @@ app_name='catalogue'
 urlpatterns = [
     path('artist/', views.artist.index, name='artist-index'),
     path('artist/<int:artist_id>', views.artist.show, name='artist-show'),
+    path('artist/edit/<int:artist_id>', views.artist.edit, name='artist-edit'),
 ]
--- reservations/catalogue/views/artist.py
+++ reservations/catalogue/views/artist.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
 from catalogue.models import Artist
+from catalogue.forms.ArtistForm import ArtistForm
 
 # Create your views here.
 def index(request):
 	artists = Artist.objects.all()
+	title = 'Liste des artistes'
 	
 	return render(request, 'artist/index.html', {
 		'artists':artists,
+		'title':title
 	})
 
 def show(request, artist_id):
@@ -16,6 +19,32 @@ def show(request, artist_id):
 	except Artist.DoesNotExist:
 		raise Http404('Artist inexistant');
 		
+	title = 'Fiche d\'un artiste'
+	
 	return render(request, 'artist/show.html', {
 		'artist':artist,
+		'title':title 
+	})
+
+def edit(request, artist_id):
+	# fetch the object related to passed id
+	artist = Artist.objects.get(id=artist_id)
+
+	
+	# pass the object as instance in form
+	form = ArtistForm(request.POST or None, instance = artist)
+	
+	if request.method == 'POST':	#TODO http_override doesn't work
+		# save the data from the form and
+		# redirect to detail_view
+		if form.is_valid():
+			form.save()
+			
+			return render(request, "artist/show.html", {
+				'artist' : artist,
+			})
+
+	return render(request, 'artist/edit.html', {
+		'form' : form,
+		'artist' : artist,
 	})
