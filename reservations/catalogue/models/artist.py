c4d26d09d701b0423543f38eb18ea7b48e009af6
marwane-alhaddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-02-18 23:58:10 +0100

    Relation ManyToMany between Artist & Type

--- reservations/catalogue/models/artist.py
+++ reservations/catalogue/models/artist.py
@@ -1,11 +1,12 @@
 # catalogue/models/artist.py
 from django.db import models
-
+from .type import *
 
 class Artist(models.Model):
     firstname = models.CharField(max_length=60)
     lastname = models.CharField(max_length=60)
-
+    types = models.ManyToManyField(Type , related_name='artists', db_table='artist_type')
+    
     def __str__(self):
         return self.firstname + " " + self.lastname
 
