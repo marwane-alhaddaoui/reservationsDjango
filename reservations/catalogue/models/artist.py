27a24622f0c745f153e0b83df4ef465f2b9b7bfe
marwane-alhaddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-02-19 00:11:38 +0100

    Données initiales ArtistType

--- reservations/catalogue/models/artist.py
+++ reservations/catalogue/models/artist.py
@@ -2,13 +2,34 @@
 from django.db import models
 from .type import *
 
+
+class ArtistManager(models.Manager):
+    def get_by_natural_key(self, firstname, lastname):
+        return self.get(firstname=firstname, lastname=lastname)
+
+
+
+
 class Artist(models.Model):
     firstname = models.CharField(max_length=60)
     lastname = models.CharField(max_length=60)
     types = models.ManyToManyField(Type , related_name='artists', db_table='artist_type')
+    
+    objects = ArtistManager()
     
     def __str__(self):
         return self.firstname + " " + self.lastname
 
     class Meta:
         db_table = 'artist'
+        
+        constraints = [
+            models.UniqueConstraint(
+                fields=["firstname", "lastname"],
+                name="unique_firstname_lastname",
+            ),
+        ]
+
+    def natural_key(self):
+        return (self.firstname, self.lastname)
+
